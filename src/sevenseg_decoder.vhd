18d05279becd1b4d2e95fd722a49d50e60f525db
AFolsom <folsomam@outlook.com>
2026-02-24 15:10:39 -0700

    XDC changes & Testbench Waveform made

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -37,8 +37,26 @@ entity sevenseg_decoder is
 end sevenseg_decoder;
 
 architecture Behavioral of sevenseg_decoder is
-
+    signal s_abcdefg_n : STD_LOGIC_VECTOR(6 downto 0);
 begin
-
-
+    with i_Hex select
+      s_abcdefg_n <=
+        "0000001" when "0000", --0
+        "1001111" when "0001", --1
+        "0010010" when "0010", --2
+        "0000110" when "0011", --3
+        "1001100" when "0100", --4
+        "0100100" when "0101", --5
+        "0100000" when "0110", --6
+        "0001111" when "0111", --7
+        "0000000" when "1000", --8
+        "0000100" when "1001", --9
+        "0001000" when "1010", --A
+        "1100000" when "1011", --b
+        "0110001" when "1100", --C
+        "1000010" when "1101", --d
+        "0110000" when "1110", --E
+        "0111000" when "1111", --F
+        "1111111" when others; --off
+    o_seg_n <= s_abcdefg_n(0) & s_abcdefg_n(1) & s_abcdefg_n(2) & s_abcdefg_n(3) & s_abcdefg_n(4) & s_abcdefg_n(5) & s_abcdefg_n(6);
 end Behavioral;
--- src/sevenseg_decoder_tb.vhd
+++ src/sevenseg_decoder_tb.vhd
@@ -24,7 +24,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -32,13 +32,25 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity sevenseg_decoder_tb is
-    Port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
-           o_seg_n : out STD_LOGIC_VECTOR (6 downto 0));
 end sevenseg_decoder_tb;
 
 architecture Behavioral of sevenseg_decoder_tb is
-
+    signal hex   : std_logic_vector(3 downto 0) := (others => '0');
+    signal seg_n : std_logic_vector(6 downto 0);
 begin
-
+    uut:entity work.sevenseg_decoder
+        port map (
+            i_Hex => hex, 
+            o_seg_n => seg_n
+           );
+    stim: process
+    begin
+        for i in 0 to 15 loop
+            hex <= std_logic_vector(to_unsigned(i,4));
+            wait for 10 ns;
+            
+        end loop;
+        wait;
+   end process;
 
 end Behavioral;
